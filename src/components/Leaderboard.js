0c9f42668f5fd278dbd189c73917392cabceaa6d
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-29 17:18:09 +0600

    leaderboard card added

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import LeaderBoardCard from './LeaderBoardCard';
 
+
+// The Leaderboard is available at / leaderboard.
+// Each entry on the leaderboard contains the following:
+// the user’s name;
+// the user’s picture;
+// the number of questions the user asked; and
+// the number of questions the user answered.
+// Users are ordered in descending order based on the sum of the number of questions they’ve answered and the number of questions they’ve asked.
 class Leaderboard extends Component {
     render() {
         return (
-            <div>
+            <Container className="text-center">
                 <h1>Leaderboard</h1>
-            </div>
+                <LeaderBoardCard />
+            </Container>
         );
     }
 }
