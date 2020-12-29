4bad20f8856de8c857912c602137e2795f9dd5bd
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-29 17:25:11 +0600

    form added

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { Container, Form, Row, Button } from 'react-bootstrap';
 
+
+// The form is available at / add.
+// The application shows the text “Would You Rather” and has a form for creating two options.
+// Upon submitting the form, a new poll is created and the user is taken to the home page.
+// The new polling question appears in the correct category on the home page.
 class AddQuestion extends Component {
     render() {
         return (
-            <div>
+            <Container className="text-center">
                 <h1>Add Question</h1>
-            </div>
+                <h2>Would You Rather</h2>
+
+                <Row>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+    </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+  </Button>
+                    </Form>
+                </Row>
+            </Container>
         );
     }
 }
