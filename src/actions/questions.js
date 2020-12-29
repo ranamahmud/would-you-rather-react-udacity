eba60bdb4c5f1546486a9d10d1f751cab9a06466
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-29 15:53:33 +0600

    css class name tweet to question

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,8 @@
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 
-export function receiveQuestions(tweets) {
+export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
-        tweets,
+        questions,
     }
 }
