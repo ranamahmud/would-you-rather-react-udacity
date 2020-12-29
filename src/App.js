9b80bbc4f71ce73473108115bbb04dc7afbe4674
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-29 15:30:47 +0600

    api added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
+import Home from './components/Home';
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <div>
+        {/* <AuthButton />
+
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul> */}
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="questions/:question_id">
+            <Question />
+          </Route>
+          <Route path="/add">
+            <AddQuestion />
+          </Route>
+          <Route path="/leaderboard">
+            <Leaderboard />
+          </Route>
+          {/* <Route path="/public">
+            <PublicPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute path="/protected">
+            <ProtectedPage />
+          </PrivateRoute> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
