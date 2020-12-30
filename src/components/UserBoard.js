04ab2e92fe448f1fb4ce20dc3bea7156370e076d
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-30 17:00:46 +0600

    leaderboard centered

--- src/components/UserBoard.js
+++ src/components/UserBoard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Container } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 export default class UserBoard extends Component {
     render() {
@@ -7,21 +7,23 @@ export default class UserBoard extends Component {
 
         return (
             <Container>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>{user.name}</Card.Title>
-                        <Card.Text>
-                            Questions answered: {Object.keys(user.answers).length}
-                        </Card.Text>
-                        <Card.Text>
-                            Questions created: {user.questions.length}
-                        </Card.Text>
-                        <Card.Text>
-                            {user.score}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <Col className="d-flex justify-content-center">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>{user.name}</Card.Title>
+                            <Card.Text>
+                                Questions answered: {Object.keys(user.answers).length}
+                            </Card.Text>
+                            <Card.Text>
+                                Questions created: {user.questions.length}
+                            </Card.Text>
+                            <Card.Text>
+                                {user.score}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
             </Container>
         )
     }
