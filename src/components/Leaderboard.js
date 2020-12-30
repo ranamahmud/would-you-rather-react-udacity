f6052b2dac34111d74981aa4980db7f582e188d2
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-30 16:45:11 +0600

    user score calculated

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
+import { connect } from 'react-redux';
 import LeaderBoardCard from './LeaderBoardCard';
 
 
@@ -12,6 +13,14 @@ import LeaderBoardCard from './LeaderBoardCard';
 // Users are ordered in descending order based on the sum of the number of questions they’ve answered and the number of questions they’ve asked.
 class Leaderboard extends Component {
     render() {
+        const { users } = this.props;
+        for (const id of Object.keys(users)) {
+            const user = users[id];
+            // console.log(user)
+            users[id].score = Object.keys(user.answers).length + user.questions.length;
+            // console.log({ user });
+            console.log(users[id]);
+        }
         return (
             <Container className="text-center">
                 <h1>Leaderboard</h1>
@@ -20,5 +29,11 @@ class Leaderboard extends Component {
         );
     }
 }
-
-export default Leaderboard;
+function mapStateToProps({ authedUser, users, questions }) {
+    return {
+        authedUser,
+        users,
+        questions
+    }
+}
+export default connect(mapStateToProps)(Leaderboard);
